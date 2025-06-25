9157f393e6bde6ce608103b44296bb478df1f99b
Javian Phillips <javianph3@gmail.com>
2025-06-25 03:35:34 -0500

    Take 2

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -1,55 +1,40 @@
 ﻿using System;
 
+
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return num1 == num2;
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            var difference = minuend - subtrahend;
-            return difference;
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            var solution = number1 + number2;
-            return solution;
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            var mininum = number1 < number2 ? number1 : number2;
-            return mininum;
+            return number1 < number2 ? number1 : number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            var product = factor1 * factor2;
-            return product;
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            nameOfPerson = Console.ReadLine();
-            var greeting = "Hello " + nameOfPerson;
-            return greeting;
+            return string.IsNullOrEmpty(nameOfPerson) ? "Hello!" : $"Hello, {nameOfPerson}!";
         }
 
-        public string GetHey()
-        {
-            return "Hey";
-        }
+        public string GetHey() => "HEY!";
     }
 }
